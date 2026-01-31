6b309c0bf916f8b3d9f06984d1e0c09b6dea71ea
Nguyễn Hữu Duy <huuduy.uwu@gmail.com>
2026-01-31 18:47:35 +0700

    thêm trang about us ở header + gắn googlemap vào trang

---
+++ HotelBookingSystem/HotelBookingSystem-main-main/src/main/java/com/hotelbookingsystem/controller/PageController.java
@@ -0,0 +1,14 @@
+package com.hotelbookingsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+}
