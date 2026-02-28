fda92763ea50a2c0a238b7d18031a6783422b168
Nguyễn Hữu Duy <huuduy.uwu@gmail.com>
2026-02-28 23:46:44 +0700

    feat: workflow4: change room type (enum)

---
+++ HotelBookingSystem/main/src/main/java/com/hotelbookingsystem/enums/ChangeRoomType.java
@@ -0,0 +1,6 @@
+package com.hotelbookingsystem.enums;
+
+public enum ChangeRoomType {
+    UPGRADE,     // Nâng hạng phòng (trả thêm tiền)
+    SAME_LEVEL   // Đổi phòng cùng cấp (không tính tiền)
+}
